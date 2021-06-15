f162709b22ca6f2096264cda1d45aa86b2f4c7c3
James Meyer <james.meyer@nebulr.net>
2021-06-15 15:44:26 -0500

    Initial commit

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -14,7 +14,7 @@ public class AppConstants {
   public static final String VERSION_NAME = "1.0.1";
   public static String INITIAL_URL = "exp://exp.host/@veritas/rosarium";
   public static final String SHELL_APP_SCHEME = "exp49e3a12ac9254761b7571b3ced47538b";
-  public static final String RELEASE_CHANNEL = "default";
+  public static final String RELEASE_CHANNEL = "sancte-rosarium";
   public static boolean SHOW_LOADING_VIEW_IN_SHELL_APP = true;
   public static boolean ARE_REMOTE_UPDATES_ENABLED = true;
   public static final List<Constants.EmbeddedResponse> EMBEDDED_RESPONSES;
@@ -23,9 +23,14 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        
+        
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@veritas/rosarium", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40veritas%2Frosarium%2F1.0.1%2F88ff782769a27b6d802fb93d2743d9a3-35.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
